c15a9b3cd16730f087f8da17722f7ab28b755f63
hularuns <159131400+hularuns@users.noreply.github.com>
2024-09-21 20:39:17 +0100

    lil bug fix with capitalisation

--- main.py
+++ main.py
@@ -12,72 +12,42 @@ class CryptoNames(str, Enum):
     solana = "SOL"
 
 class CryptoAnalysis:
-    def __init__(self, simulation_length:int = 60, crypto: CryptoNames = CryptoNames.litecoin.value) -> None:
-        self.simulation_length = simulation_length
-        self.crypto = crypto
-        self.crypto_data = self.get_daily_prices()
-        self.weekdays = {0: "monday", 1: "tuesday", 2: "wednesday", 3: "thursday", 4: "friday", 5: "saturday", 6: "sunday"}
-        self.assign_weekdays()
+    def __init__(self ) -> None:
         pass
     
-    def get_daily_prices(self):
+    def get_daily_prices(self, crypto:CryptoNames = CryptoNames.litecoin.value, days: int = 60):
         """ Gets daily prices of open and close at midnight in usd"""
-        url = f"https://data-api.cryptocompare.com/index/cc/v1/historical/days?market=cadli&instrument={self.crypto}-USD&limit={self.simulation_length}&aggregate=1&fill=true&apply_mapping=true&response_format=JSON"
+        url = f"https://data-api.cryptocompare.com/index/cc/v1/historical/days?market=cadli&instrument={crypto}-USD&limit={days}&aggregate=1&fill=true&apply_mapping=true&response_format=JSON"
         request = requests.get(url).json()
         request_data = request.get('Data', None)
         return request_data
-        
-    # def extract_mondays_fridays(self) -> dict:
-    #     mondays = []
-    #     fridays = []
-        
-    #     for day in self.crypto_data:
-    #         timestamp = day.get("TIMESTAMP", None)
-    #         weekday = datetime.fromtimestamp(timestamp=timestamp, tz=pytz.utc).weekday()
-    #         if timestamp is not None:
-    #             if weekday == 0:
-    #                 mondays.append(day)
-    #             elif weekday == 4:
-    #                 fridays.append(day)
-        
-    #     dict_to_return = {
-    #         "mondays": mondays,
-    #         "fridays": fridays
-    #     }
-    #     return dict_to_return
-    
-    # def get_initial_weekday(self) -> int: 
-    #     """Returns the initial weekday 0->6 Mon->Sun the data starts on."""       
-    #     initial_weekday = 0
-    #     for day in self.crypto_data:
-    #         weekday = datetime.fromtimestamp(timestamp=day.get("TIMESTAMP", None), tz = pytz.utc).weekday()
-    #         initial_weekday = weekday if weekday > initial_weekday else initial_weekday
-    #     return initial_weekday
            
-    def assign_weekdays(self) -> None:
+    def assign_weekdays(self, raw_data: dict) -> None:
         """ Assigns weekdays as day names and also week number as if from start of data, calendar week and formatted date. """
+        weekdays = {0: "monday", 1: "tuesday", 2: "wednesday", 3: "thursday", 4: "friday", 5: "saturday", 6: "sunday"}      
         week_number = 1
-        for day in self.crypto_data:
+        for day in raw_data:
             timestamp = day.get("TIMESTAMP", None)
             datetime_obj = datetime.fromtimestamp(timestamp=timestamp, tz = pytz.utc)   
             weekday = datetime_obj.weekday()
             
             if timestamp is not None:
                 day.update({
-                    "weekday": self.weekdays.get(weekday, None),
+                    "weekday": weekdays.get(weekday, None),
                     "week": week_number,
                     "calendar_week": datetime_obj.isocalendar()[1],
                     "date": datetime_obj.strftime('%Y-%m-%d')
                 })
                 week_number += 1 if weekday >= 6 else 0
+        return raw_data
         
-    def identify_buy_sell_days(self, purchase_day: str = "monday", sell_day: str = "friday"):      
-        purchase_day_data = []
-        sell_day_data = []       
-        weekly_data = {}
-        
-        for day in self.crypto_data:
+    def identify_buy_sell_days(self, raw_data: dict, purchase_day: str = "monday", sell_day: str = "friday"):
+        """ Get rid of this and handle is entirely within simulation method... this is inefficient but works. """
+        weekly_data = {} 
+        pprint(raw_data)
+        for day in raw_data:
             if day['weekday'] == purchase_day:
+
                 weekly_data.setdefault(day['week'], {}).update({
                     "purchase": {
                     "low_price": day['LOW'],
@@ -95,13 +65,19 @@ class CryptoAnalysis:
                         "weekday": day['weekday']
                         }
                 })
-        
+
         return weekly_data
+    
+    def get_simulation_data(self, purchase_day: str ="monday", sell_day: str = "friday", crypto: CryptoNames.litecoin.value = "litecoin", days: int = 60):
+        raw_data = self.get_daily_prices(crypto=crypto, days=days)
+        week_day_data = self.assign_weekdays(raw_data=raw_data)
+        filtered_data = self.identify_buy_sell_days(raw_data=week_day_data, purchase_day=purchase_day, sell_day=sell_day)
+        return filtered_data
             
-    def simulate_weekly_process(self, starting_usd=100, purchase_day:str = "monday", sell_day: str = "friday", tx_tax: float = 0.004):
+    def simulate_weekly_process(self, starting_usd=100, purchase_day:str = "monday", sell_day: str = "friday", crypto: CryptoNames.litecoin.value = "litecoin", days: int = 60, tx_tax: float = 0.004):
         """ purchase from usd into crypto funds """
-        weekly_data = self.identify_buy_sell_days(purchase_day=purchase_day.lower(), sell_day=sell_day.lower())
-        
+        weekly_data = self.get_simulation_data(purchase_day=purchase_day.lower(), sell_day=sell_day.lower(), crypto=crypto, days=days)
+ 
         usd_funds = starting_usd
         previous_money = usd_funds
         weekly_price_diff = {}
@@ -111,7 +87,7 @@ class CryptoAnalysis:
         for week, data in weekly_data.items():
             
             if "purchase" in data and "sell" in data:
-                purchase_price = data.get('purchase', {}).get('high_price', None)
+                purchase_price = data.get('purchase', {}).get('low_price', None)
                 held_crypto = (usd_funds / purchase_price) * (1-tx_tax) #taxed purchase
                 usd_funds = 0 # set to 0 as spent.
 
@@ -131,21 +107,22 @@ class CryptoAnalysis:
                 weekly_price_diff.setdefault(week, {}).update({
                     "purchase_price (high)": purchase_price,
                     "sell_price (low)": sell_price,
-                    "tax": tx_tax,
+                    "tax": f"{tx_tax*100}%",
                     "profit": profit,
                     "profit_or_loss": profit_or_loss,
                     "simulated_value_after_tax": usd_funds
                 })
                 previous_money = usd_funds
         print(f"Total number of profit weeks: {profit_week}, total number of loss weeks: {loss_week}")
-        print(f"Starting with ${starting_usd}, after {self.simulation_length} days across {profit_week+loss_week} weeks, held funds are now worth ${usd_funds:.2f}.")
+        print(f"Starting with ${starting_usd}, after {days} days across {profit_week+loss_week} weeks with trading tax of: {tx_tax*100}%, held funds are now worth ${usd_funds:.2f} .")
+        print(weekly_price_diff)
         return weekly_price_diff
     
 if __name__ == "__main__":
-    crypto_analysis = CryptoAnalysis(simulation_length = 50, crypto= CryptoNames.litecoin.value)
+    crypto_analysis = CryptoAnalysis()
     data = crypto_analysis.get_daily_prices()
     # crypto_analysis.price_difference_between_two_days()
-    simulation = crypto_analysis.simulate_weekly_process(starting_usd = 100, purchase_day = "Monday", sell_day = "Friday")
+    simulation = crypto_analysis.simulate_weekly_process(starting_usd = 100, purchase_day = "Monday", sell_day = "thursday",crypto=CryptoNames.litecoin.value, days = 10, tx_tax=0.004)
     pprint(simulation)
     # random_date = datetime.fromtimestamp(timestamp=1726704000, tz=pytz.utc).weekday()
     # print(random_date)
